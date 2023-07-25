e7c818141a52d0deb56acbe4c3330e799214f5a2
artemiikulikov <aapqwhat@gmail.com>
2023-07-25 13:47:39 +0300

    :shirt: update: add basic cache implementation

--- main.go
+++ main.go
@@ -1,20 +1,48 @@
 package main
 
 import (
-    "net/http"
+//	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
 )
 
-func handleSlothfulMessage(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add("Content-Type", "application/json")
-    w.Write([]byte(`{"message": "Stay slothful!"}`))
+type cacheHandler struct {
+	mu sync.Mutex // guards n
+	cache map[string]string
 }
+
+func (c *cacheHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  c.mu.Lock()
+  defer c.mu.Unlock()
+  v, ok := c.cache["example"]
+  w.Header().Add("Content-Type", "application/json")
+  fmt.Println(v)
+  if ok {
+    w.Write([]byte(v))
+    fmt.Println(v)
+  } else {
+    // request data
+    w.Write([]byte(`{"message": "helloWorld"}`))
+    c.cache["example"] = `{"message": "cache stored"}`
+  }
+}
+
+func handleHelloWorld(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Content-Type", "application/json")
+    w.Write([]byte(`{"message": "helloWorld"}`))
+}
+
 
 func appRouter() http.Handler {
     rt := http.NewServeMux()
-    rt.HandleFunc("/sloth", handleSlothfulMessage)
+    // rt.HandleFunc("/helloworld", handleHelloWorld)
     return rt
 }
 
 func main() {
-  http.ListenAndServe(":8080", appRouter()) 
+  cacheHandler := new(cacheHandler)
+  cacheHandler.cache = make(map[string]string)
+  http.Handle("/data", cacheHandler)
+  http.ListenAndServe(":8080", nil) 
 }
--- server_test.go
+++ server_test.go
@@ -7,16 +7,16 @@ import (
     "testing"
 )
 
-func TestHandleSlothfulMessage(t *testing.T) {
+func TestHandleHelloWorld(t *testing.T) {
     wr := httptest.NewRecorder()
     req := httptest.NewRequest(http.MethodGet, "/sloth", nil)
 
-    handleSlothfulMessage(wr, req)
+    handleHelloWorld(wr, req)
     if wr.Code != http.StatusOK {
         t.Errorf("got HTTP status code %d, expected 200", wr.Code)
     }
 
-    if !strings.Contains(wr.Body.String(), "Stay slothful!") {
+    if !strings.Contains(wr.Body.String(), "helloWorld") {
         t.Errorf(
             `response body "%s" does not contain "Stay slothful!"`,
             wr.Body.String(),
