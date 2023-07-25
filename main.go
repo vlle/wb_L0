cf65ec7cfdf36b463048c94af37b2e0be54e54c9
artemiikulikov <aapqwhat@gmail.com>
2023-07-25 13:00:22 +0300

    :heavy_minus_sign: rm docker files

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "net/http"
+)
+
+func handleSlothfulMessage(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Content-Type", "application/json")
+    w.Write([]byte(`{"message": "Stay slothful!"}`))
+}
+
+func appRouter() http.Handler {
+    rt := http.NewServeMux()
+    rt.HandleFunc("/sloth", handleSlothfulMessage)
+    return rt
+}
+
+func main() {
+  http.ListenAndServe(":8080", appRouter()) 
+}
